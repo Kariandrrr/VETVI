8ca48638555a41ad25d8a0ffd8faf43ecea47222
Kariandrrr <educationkarina08@gmail.com>
2026-04-30 19:51:04 +0300

    hooks for family

---
+++ src/hooks/useFamilies.ts
@@ -0,0 +1,42 @@
+import { useMutation, useQuery, useQueryClient } from '@tanstack/react-query';
+import { familyApi } from '@/api/family';
+import type {InvitationCreate} from '@/types/families';
+
+export const useFamilies = () => {
+  return useQuery({
+    queryKey: ['families'],
+    queryFn: familyApi.getMyFamilies,
+  });
+};
+
+export const useCreateFamily = () => {
+  const queryClient = useQueryClient();
+
+  return useMutation({
+    mutationFn: familyApi.createFamily,
+    onSuccess: () => {
+      queryClient.invalidateQueries({ queryKey: ['families'] });
+    },
+  });
+};
+
+export const useCreateInvite = () => {
+  const queryClient = useQueryClient();
+
+  return useMutation({
+    mutationFn: ({ familyId, data }: { familyId: string; data: InvitationCreate }) =>
+      familyApi.createInvite(familyId, data),
+    onSuccess: () => {
+      queryClient.invalidateQueries({ queryKey: ['families'] });
+    },
+  });
+};
+
+export const useDeleteFamily = () => {
+  const queryClient = useQueryClient();
+
+  return useMutation({
+    mutationFn: familyApi.deleteFamily,
+    onSuccess: () => queryClient.invalidateQueries({ queryKey: ['families'] }),
+  });
+};
