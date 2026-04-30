9c9a3bc8dfefbdd3f3b26b30797521a35d1f4167
Kariandrrr <educationkarina08@gmail.com>
2026-04-30 19:51:42 +0300

    add routers

--- src/App.tsx
+++ src/App.tsx
@@ -1,28 +1,48 @@
 import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
-import { AuthProvider } from '@/context/AuthContext.tsx';
+import { AuthProvider } from '@/context/AuthContext';
 import { RegisterPage } from '@/pages/RegisterPage';
 import { LoginPage } from '@/pages/LoginPage';
 import { HomePage } from '@/pages/HomePage';
+import { FamiliesPage } from '@/pages/FamiliesPage';
 import { ProtectedRoute } from '@/components/ProtectedRoute';
+import { JoinPage } from '@/pages/JoinPage';
+
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+
+const queryClient = new QueryClient();
 
 function App() {
   return (
     <Router>
-      <AuthProvider>
-        <Routes>
-          <Route path="/register" element={<RegisterPage />} />
-          <Route path="/login" element={<LoginPage />} />
-          <Route
-            path="/"
-            element={
-              <ProtectedRoute>
-                <HomePage />
-              </ProtectedRoute>
-            }
-          />
-          <Route path="*" element={<Navigate to="/" />} />
-        </Routes>
-      </AuthProvider>
+      <QueryClientProvider client={queryClient}>
+        <AuthProvider>
+          <Routes>
+            <Route path="/register" element={<RegisterPage />} />
+            <Route path="/login" element={<LoginPage />} />
+              <Route path="/join/:token" element={<JoinPage />} />
+
+            <Route
+              path="/families"
+              element={
+                <ProtectedRoute>
+                  <FamiliesPage />
+                </ProtectedRoute>
+              }
+            />
+
+            <Route
+              path="/"
+              element={
+                <ProtectedRoute>
+                  <HomePage />
+                </ProtectedRoute>
+              }
+            />
+
+            <Route path="*" element={<Navigate to="/" replace />} />
+          </Routes>
+        </AuthProvider>
+      </QueryClientProvider>
     </Router>
   );
 }
