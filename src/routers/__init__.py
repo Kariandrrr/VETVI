a67f41306a39c822c5576c8fe5b4f773540dc838
Kariandrrr <educationkarina08@gmail.com>
2026-05-09 19:23:22 +0300

    refactor main a bit. now all  routers are in the init file

--- src/routers/__init__.py
+++ src/routers/__init__.py
@@ -6,12 +6,18 @@ from .join import router as join_router
 from .log_in import router as log_in_router
 from .members.members import router as members_router
 from .members.relationships import router as members_relationships_router
+from .favourite_family_group import router as favourite_family_group_router
 
 router = APIRouter()
 
-router.include_router(log_in_router)
-router.include_router(families_router)
-router.include_router(invitations_router)
-router.include_router(join_router)
-router.include_router(members_relationships_router)
-router.include_router(members_router)
+router.include_router(log_in_router, prefix="/auth", tags=["register"])
+router.include_router(families_router, prefix="/families", tags=["families"])
+router.include_router(invitations_router, prefix="/families", tags=["invitations"])
+router.include_router(join_router, prefix="/join", tags=["join"])
+router.include_router(members_router, prefix="/families", tags=["members"])
+router.include_router(
+    members_relationships_router, prefix="/families", tags=["relationships"]
+)
+router.include_router(
+    favourite_family_group_router, prefix="/families", tags=["favourite"]
+)
