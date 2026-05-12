5c1e5c08bba426640905d4a44a18770641813d95
Kariandrrr <educationkarina08@gmail.com>
2026-05-12 12:07:24 +0300

    add routers

--- src/routers/__init__.py
+++ src/routers/__init__.py
@@ -8,6 +8,12 @@ from .members.members import router as members_router
 from .members.relationships import router as members_relationships_router
 from .favourite_family_group import router as favourite_family_group_router
 
+from .posts_and_profiles.media import router as posts_and_profiles_media_router
+from .posts_and_profiles.posts import router as posts_and_profiles_posts_router
+from .posts_and_profiles.profiles import router as posts_and_profiles_profiles_router
+from .posts_and_profiles.reactions import router as posts_and_profiles_reactions_router
+from .posts_and_profiles.tags import router as posts_and_profiles_tags_router
+
 router = APIRouter()
 
 router.include_router(log_in_router, prefix="/auth", tags=["register"])
@@ -21,3 +27,14 @@ router.include_router(
 router.include_router(
     favourite_family_group_router, prefix="/families", tags=["favourite"]
 )
+
+# posts_and_profiles
+router.include_router(posts_and_profiles_media_router, prefix="/media", tags=["posts"])
+router.include_router(posts_and_profiles_posts_router, prefix="/posts", tags=["posts"])
+router.include_router(
+    posts_and_profiles_profiles_router, prefix="/profiles", tags=["profiles"]
+)
+router.include_router(
+    posts_and_profiles_reactions_router, prefix="/reactions", tags=["posts"]
+)
+router.include_router(posts_and_profiles_tags_router, prefix="/tags", tags=["posts"])
